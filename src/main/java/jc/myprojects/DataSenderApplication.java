3b93bc1e6e6015fb39a1404c735dceef91017190
jaroslaw.ciarkowski <jaroslaw.ciarkowski@gmail.com>
2017-04-15 16:02:48 +0200

    Init commit

---
+++ src/main/java/jc/myprojects/DataSenderApplication.java
@@ -0,0 +1,13 @@
+package jc.myprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSenderApplication.class, args);
+	}
+
+}
